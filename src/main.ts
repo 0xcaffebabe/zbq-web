7fc3f683179980601c4800bcc3efe320df6e7dd4
MY <715711877@qq.com>
2021-04-14 21:26:10 +0800

    项目搭建

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import {
+  createRouter, createWebHashHistory
+} from 'vue-router'
+import 'element-plus/lib/theme-chalk/index.css'
+
+import LoginPage from '@/views/LoginPage.vue'
+const routes = [
+  { path: '/login', component: LoginPage }
+]
+const router = createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: createWebHashHistory(),
+  routes, // `routes: routes` 的缩写
+})
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    this is login page
+    <register></register>
+  </div>
+</template>
+
+<script lang="ts">
+import Register from '@/components/Register.vue'
+export default {
+  name: 'LoginPage',
+  components: {
+    Register
+  }
+}
+</script>
+
+<style>
+
+</style>
